bd39f7ef97938049365d616a15ea931bcff03918
NiveditaSachan <nivedita.hbti@gmail.com>
2018-09-05 10:55:43 +0530

    Update iOSObjectClass.java

--- pageFactory/iOSObjectClass.java
+++ pageFactory/iOSObjectClass.java
@@ -15,30 +15,30 @@ import io.appium.java_client.pagefactory.iOSFindBy;
 public class iOSObjectClass {
 	AppiumDriver driver;	
 //	String lemoneyPin="le.money:id/pin_1";
-	String lemoneyPin="//XCUIElementTypeApplication[@name=\"Lemoney\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[1]/XCUIElementTypeOther/XCUIElementTypeStaticText[2]";
-//
-//@iOSFindBy(xpath="//XCUIElementTypeApplication[@name=\"Lemoney\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[1]/XCUIElementTypeOther/XCUIElementTypeStaticText[2]")
-//@AndroidFindBy(id="le.money:id/pin_1")
-//MobileElement lemoneyPin;	
-////
+//	String lemoneyPin="//XCUIElementTypeApplication[@name=\"Lemoney\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[1]/XCUIElementTypeOther/XCUIElementTypeStaticText[2]";
+
+@iOSFindBy(xpath="//XCUIElementTypeApplication[@name=\"Lemoney\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeCollectionView/XCUIElementTypeCell[1]/XCUIElementTypeOther/XCUIElementTypeStaticText[2]")
+@AndroidFindBy(id="le.money:id/pin_1")
+MobileElement lemoneyPin;	
+
 
 public void enterPin(String Pin) {
-	driver.findElement(By.xpath(lemoneyPin)).sendKeys(Pin);
-//	lemoneyPin.sendKeys(Pin);
+//	driver.findElement(By.xpath(lemoneyPin)).sendKeys(Pin);
+	lemoneyPin.sendKeys(Pin);
 }
 
 	
 public iOSObjectClass(AppiumDriver driver) {
 	this.driver=driver;
-//	try {
-//	PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-//	}
-//	
-//	catch(ExceptionInInitializerError e) {
-//		System.out.println("e.toString() : "+ e.toString());
-//		System.out.println("e.getCause()getMessage : "+ e.getCause().getMessage());
-//
-//	}
+	try {
+	PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+	
+	catch(ExceptionInInitializerError e) {
+		System.out.println("e.toString() : "+ e.toString());
+		System.out.println("e.getCause()getMessage : "+ e.getCause().getMessage());
+
+	}
 		
 }
 
